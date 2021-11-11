74709858868d421ab275a97b993c19cbda7e5c5a
Jihyo Jeon <jihyo@gm.gist.ac.kr>
2021-11-11 19:14:59 +0000

    feat(sidebar): get notes list

--- client/components/Sidebar.tsx
+++ client/components/Sidebar.tsx
@@ -1,21 +1,28 @@
 import Link from 'next/link';
 import styles from './Sidebar.module.css';
 
-function resize() {
-  const sidebarDiv = document.getElementsByClassName(styles.side)[0];
-  const orgWidth: number = sidebarDiv
-    ? sidebarDiv.getBoundingClientRect().width
-    : 0;
-  console.log(orgWidth);
-}
+export default ({ notes }) => {
+  const resize = () => {
+    const sidebarDiv = document.getElementsByClassName(styles.side)[0];
+    const orgWidth: number = sidebarDiv
+      ? sidebarDiv.getBoundingClientRect().width
+      : 0;
+    console.log(orgWidth);
+  };
 
-export default () => {
+  const noteList = (notes: []) => {
+    if (notes) {
+      return notes.map((note) => <p key={note._id}>{note.title}</p>);
+    }
+  };
+
   return (
     <div className={styles.component}>
       <div className={styles.side}>
         <div className={styles.info}>User Info</div>
-        <div className={styles.note}>notes</div>
+        <div className={styles.note}>{noteList(notes)}</div>
         <button className={styles.new}>new note</button>
+        {/* https://www.npmjs.com/package/react-beautiful-dnd-next */}
       </div>
       <div className={styles.border} onClick={resize}></div>
     </div>
