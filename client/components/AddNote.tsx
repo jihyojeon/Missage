c5980c3511c9ddc6cf9d0b25eb4128cabb8e70bf
Jihyo Jeon <jihyo@gm.gist.ac.kr>
2021-11-13 10:42:35 +0000

    feat(addnote): add note with drag and drop

--- client/components/AddNote.tsx
+++ client/components/AddNote.tsx
@@ -1,13 +1,12 @@
-import Link from 'next/link';
 import styles from './AddNote.module.css';
 import React from 'react';
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 import {
   faFileUpload,
   faMicrophoneAlt,
 } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import Router from 'next/router';
+import { useDropzone } from 'react-dropzone';
 
 export default ({ postNote }) => {
   const [audio, setAudio] = useState(null);
@@ -25,7 +24,6 @@ export default ({ postNote }) => {
     if (userAudio) {
       const formData = new FormData();
       formData.append('audio', userAudio, userAudio.name);
-      console.log(formData);
       const newNote = await postNote(formData);
     }
   };
@@ -34,17 +32,46 @@ export default ({ postNote }) => {
     document.getElementsByClassName(styles.file)[0].click();
   };
 
+  const onDrop = useCallback((acceptedFiles) => {
+    console.log(acceptedFiles);
+    send(acceptedFiles[0]);
+  }, []);
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
   return (
     <div className={styles.options}>
-      <div className={styles.drag}>
-        <p className={styles.title}>Drag and Drop | Upload</p>
-        <input type="file" className={styles.file} onChange={upload} />
-        <FontAwesomeIcon
-          onClick={trigger}
-          icon={faFileUpload}
-          className={styles.icon}
-        ></FontAwesomeIcon>
-      </div>
+      {isDragActive ? (
+        <div {...getRootProps()} className={styles.dragActive}>
+          <p className={styles.title}>Drag and Drop</p>
+          <input
+            {...getInputProps()}
+            type="file"
+            className={styles.file}
+            onChange={upload}
+          />
+          <FontAwesomeIcon
+            onClick={trigger}
+            icon={faFileUpload}
+            className={styles.icon}
+          ></FontAwesomeIcon>
+        </div>
+      ) : (
+        <div {...getRootProps()} className={styles.drag}>
+          <p className={styles.title}>Drag and Drop</p>
+          <input
+            {...getInputProps()}
+            type="file"
+            className={styles.file}
+            onChange={upload}
+          />
+          <FontAwesomeIcon
+            onClick={trigger}
+            icon={faFileUpload}
+            className={styles.icon}
+          ></FontAwesomeIcon>
+        </div>
+      )}
       <div className={styles.record}>
         <p className={styles.title}>Record</p>
         <FontAwesomeIcon
