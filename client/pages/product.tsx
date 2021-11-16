635a307d062b49bf506f6b267dcfe639d2bdf541
Jihyo Jeon <jihyo@gm.gist.ac.kr>
2021-11-16 13:47:08 +0000

    feat(font): add global font

--- client/pages/product.tsx
+++ client/pages/product.tsx
@@ -142,6 +142,13 @@ const Named = () => {
         </p>
       </p>
       <div className={styles.bottom}>
+        <img
+          className={styles.logo}
+          src={
+            'https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2Fadadf789-61b7-46b3-842c-15ea664efab4%2Flogo_crop.png?table=block&id=89d15df5-c523-42a0-962a-4fd639448767&spaceId=d6c7f512-633e-4709-ac5f-bc0979392ab1&width=1920&userId=862b32cc-9b45-426d-b64b-9590ffe306bf&cache=v2'
+          }
+          alt=""
+        />
         <Link href="/api/login">
           <a className={styles.try}> ➡️ Try Missage Free ⬅️ </a>
         </Link>
